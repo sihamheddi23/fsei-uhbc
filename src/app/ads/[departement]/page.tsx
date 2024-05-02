a47d62e0ae1728796fbbb0dff2bb68868fd5308b
siham <siham.heddi.dev@gmail.com>
2024-05-02 09:25:15 +0100

    fix the param type

--- src/app/ads/[departement]/page.tsx
+++ src/app/ads/[departement]/page.tsx
@@ -2,10 +2,12 @@ import AdvertisingItem from '@/components/MainSections/SubSections/AdvertisingIt
 import { DEPARTEMENTS } from '@/utils/const'
 import React from 'react'
 
-function page({params}:any) {
+function page({ params }: any) {
+  const param: 'informatics' | 'maths' | 'physics' | 'chimics' = params.departement
+  const departement = DEPARTEMENTS[param]
   return (
     <div className='mx-20'>
-        <h1 className='text-2xl font-bold'>{DEPARTEMENTS[params.departement]}</h1>
+        <h1 className='text-2xl font-bold'>{departement}</h1>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7 my-7 ">
             <AdvertisingItem/>
             <AdvertisingItem/>
