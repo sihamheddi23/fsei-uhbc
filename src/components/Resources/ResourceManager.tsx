bc14ed702965fb3c2864ac4ade09e4d7d9181a24
siham <siham.heddi.dev@gmail.com>
2024-05-10 12:12:51 +0100

    resource manager

--- src/components/Resources/ResourceManager.tsx
+++ src/components/Resources/ResourceManager.tsx
@@ -159,34 +159,34 @@ const ResourceManager: React.FC<ResourceManagerProps> = ({
       
       const result = await addrow(formData, token);
       
-      // if (result) {
-      //   if (result.error) {
-      //     toast.error(result?.message.join(" , "), {
-      //       position: "top-right",
-      //       autoClose: 5000,
-      //       hideProgressBar: true,
-      //       closeOnClick: true,
-      //       pauseOnHover: true,
-      //       draggable: false,
-      //       progress: undefined,
-      //       theme: "colored",
-      //     });
-      //   }
-      //   else {
-      //     closeModal("ADD");
-      //     toast.success("لقد تمت العملية بنجاح", {
-      //       position: "top-right",
-      //       autoClose: 5000,
-      //       hideProgressBar: true,
-      //       closeOnClick: true,
-      //       pauseOnHover: true,
-      //       draggable: false,
-      //       progress: undefined,
-      //       theme: "colored",
-      //     });
-      //     window.location.reload();
-      //   }
-      // }
+      if (result) {
+        if (result.error) {
+          toast.error(result?.message.join(" , "), {
+            position: "top-right",
+            autoClose: 5000,
+            hideProgressBar: true,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: false,
+            progress: undefined,
+            theme: "colored",
+          });
+        }
+        else {
+          closeModal("ADD");
+          toast.success("لقد تمت العملية بنجاح", {
+            position: "top-right",
+            autoClose: 5000,
+            hideProgressBar: true,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: false,
+            progress: undefined,
+            theme: "colored",
+          });
+          window.location.reload();
+        }
+      }
     }
   };
 
